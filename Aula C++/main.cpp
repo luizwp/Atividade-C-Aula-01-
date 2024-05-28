46e73e69f87bb0d54668a6c32947356e9ba28dae
luizwp <148246609+luizwp@users.noreply.github.com>
2024-05-27 21:41:29 -0300

    Add files via upload

---
+++ Aula C++/main.cpp
@@ -0,0 +1,13 @@
+// Inclui a biblioteca de entrada e saída padrão do C++
+#include <iostream>
+
+// Função principal do programa, onde a execução começa
+int main() {
+    // Usa o objeto 'cout' do namespace 'std' para imprimir na tela
+    std::cout << "Hello World" << std::endl;
+
+    // Retorna 0 para indicar que o programa terminou corretamente
+    return 0;
+}
+
+// ASS: Luiz Felipe Freitas Andrade
